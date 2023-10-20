7b9732bff9a3b4425c967b5954746f1d7cdf78cc
Jav1erRam0s <ramos.aranibar.javier@gmail.com>
2023-10-20 15:11:00 -0300

    validacion de campo cantidad

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,30 @@ const fieldTotal = document.getElementById("field-total");
 
 const precioTicket = 200;
 
+const msjForm = document.getElementById("msj-form");
+
 btnBorrar.addEventListener("click", function (evento) {
   fieldForm.forEach((input) => (input.value = ""));
+  fieldTotal.textContent = 0;
 });
 
 btnResumen.addEventListener("click", function (evento) {
   const cantidad = fieldCantidad.value;
   const categoria = fieldCategoria.value;
+
+  // Validacion de campo cantidad
+
+  const cantidadValidacion = parseFloat(cantidad);
+  const esEntero = Number.isInteger(cantidadValidacion);
+
+  if (isNaN(cantidadValidacion) || cantidadValidacion < 1 || esEntero == false) {
+    mostrarMensaje(true, "⚠️ Cantidad ingresada incorrecta ⚠️");
+    return;
+  }
+
+  mostrarMensaje(false, "✅ Operacion Exitosa ✅");
+
+  // Calculo de descuento
 
   const subtotal = cantidad * precioTicket;
   var total;
@@ -35,3 +52,20 @@ btnResumen.addEventListener("click", function (evento) {
 
   fieldTotal.textContent = total;
 });
+
+function mostrarMensaje(error, mensaje) {
+  const respuesta = document.createElement("span");
+  respuesta.textContent = mensaje;
+
+  if (error) {
+    msjForm.classList.add("alert", "alert-danger");
+  } else {
+    msjForm.classList.add("alert", "alert-success");
+  }
+
+  msjForm.appendChild(respuesta);
+  setTimeout(() => {
+    msjForm.classList.remove("alert", "alert-success", "alert-danger");
+    respuesta.remove();
+  }, 5000);
+}
